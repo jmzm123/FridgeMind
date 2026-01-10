bcf4c5884300829a08b79d0890836bd0d15a924f
陈佳欣 <akkoooookka@168.com>
2026-01-10 17:01:50 +0800

    pm2正常运行

---
+++ server/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'fridgemind-server',
+    script: 'dist/server.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    },
+    log_file: './logs/combined.log',
+    out_file: './logs/out.log',
+    error_file: './logs/error.log',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+  }]
+};
